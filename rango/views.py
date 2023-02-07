6d1a654f39e0f68130b5e6ebfe6e3f798de56fc1
Marvelousds <triggermao@gmail.com>
2023-02-07 12:03:45 +0800

    update name

--- rango/views.py
+++ rango/views.py
@@ -26,7 +26,7 @@ def index(request):
 
 def about(request):
     # return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
-    context_dict = {'boldmessage': 'This tutorial has been put together by <your-name>'}
+    context_dict = {'boldmessage': 'This tutorial has been put together by Changcheng Mao'}
     return render(request, 'rango/about.html', context=context_dict)
 
 
