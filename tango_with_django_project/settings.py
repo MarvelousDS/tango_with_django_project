98c63e1787d608a57823dcc3812a3974f2629085
Marvelousds <triggermao@gmail.com>
2023-02-07 22:09:08 +0800

    finish 9.6

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -36,6 +36,17 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+)
+
+# PASSWORD_HASHERS = [
+#     'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+#     'django.contrib.auth.hashers.BCryptPasswordHasher',
+#     'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+#     'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+# ]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -99,6 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {'min_length': 6, }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
